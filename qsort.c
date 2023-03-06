a1f07ef396291445e00235bd6e234d04f294cbad
José Hurtarte <jjhh121106@gmail.com>
2023-03-06 01:39:29 -0600

    made the quick sort faster when parallel using tasks

--- qsort.c
+++ qsort.c
@@ -1,54 +1,71 @@
-// Ejemplo de QuickSort
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #include <omp.h>
 
-void par_qsort(int *data, int lo, int hi)
-{
-  if (lo >= hi) return; // base case
-  int l = lo;
-  int h = hi;
-  int p = data[(hi + lo)/2];
-  while(l <= h){
-    while(data[l] < p) l++;
-    while(data[h] > p) h--;
-    if(l <= h){
-      //swap
-      int tmp = data[l];
-      data[l] = data[h];
-      data[h] = tmp;
-      l++; h--;
-    }
-  }
-  //recursive call
-  # pragma omp task shared(data)
-  par_qsort(data, lo, h);
-  # pragma omp task shared(data)
-  par_qsort(data, l, hi);
+#define TREESHOLD 10000
 
-  # pragma omp taskwait
+int compare(const void * a, const void * b) {
+    return (*(int*)a - *(int*)b);
+}
+
+void par_qsort(int *data, int lo, int hi) {
+    if (lo >= hi) return;
+
+    int l = lo, h = hi, p = data[(hi + lo) / 2];
+    while (l <= h) {
+        while (data[l] < p) l++;
+        while (data[h] > p) h--;
+        if (l <= h) {
+            int tmp = data[l];
+            data[l] = data[h];
+            data[h] = tmp;
+            l++; h--;
+        }
+    }
+
+    if ((h - lo) > TREESHOLD) {
+        #pragma omp task shared(data) firstprivate(lo, h)
+        par_qsort(data, lo, h);
+    } else {
+        qsort(&data[lo], h - lo + 1, sizeof(int), compare);
+    }
+
+    if ((hi - l) > TREESHOLD) {
+        #pragma omp task shared(data) firstprivate(l, hi)
+        par_qsort(data, l, hi);
+    } else {
+        qsort(&data[l], hi - l + 1, sizeof(int), compare);
+    }
+
+    #pragma omp taskwait
 }
 
 int main() {
-    int n = 10000000;
+    int n = 60;
     int *data = malloc(n * sizeof(int));
-    if (data == NULL) {
-        printf("Failed to allocate memory for data\n");
-        return 1;
-    }
     for (int i = 0; i < n; i++) {
-      data[i] = rand() % 100;
+        data[i] = rand() % 100;
     }
+
     double start, end;
     start = omp_get_wtime();
-    # pragma omp parallel shared(data) num_threads(1)
+
+    #pragma omp parallel shared(data) num_threads(4)
     {
-      # pragma omp single
-      par_qsort(data, 0, n-1);
+        #pragma omp single
+        par_qsort(data, 0, n - 1);
     }
+
     end = omp_get_wtime();
-    printf("Time: %f \n", end - start);
+
+    printf("Time: %f\n", end - start);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", data[i]);
+    }
+    
+
     free(data);
     return 0;
 }
