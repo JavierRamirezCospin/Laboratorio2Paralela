61c5c05ffbea60211595570e8f40e5078e57b077
José Hurtarte <jjhh121106@gmail.com>
2023-03-06 01:48:50 -0600

    finished quicksort adjustements

--- qsort.c
+++ qsort.c
@@ -8,6 +8,28 @@
 int compare(const void * a, const void * b) {
     return (*(int*)a - *(int*)b);
 }
+void naive_qsort(int *data, int lo, int hi) //}, int (*compare)(const int *, const int*))
+{
+  if(lo > hi) return;
+  int l = lo;
+  int h = hi;
+  int p = data[(hi + lo)/2];
+
+  while(l <= h){
+    while((data[l] - p) < 0) l++;
+    while((data[h] - p) > 0) h--;
+    if(l<=h){
+      //swap
+      int tmp = data[l];
+      data[l] = data[h];
+      data[h] = tmp;
+      l++; h--;
+    }
+  }
+  //recursive call
+  naive_qsort(data, lo, h);
+  naive_qsort(data, l, hi);
+}
 
 void par_qsort(int *data, int lo, int hi) {
     if (lo >= hi) return;
@@ -28,21 +50,21 @@ void par_qsort(int *data, int lo, int hi) {
         #pragma omp task shared(data) firstprivate(lo, h)
         par_qsort(data, lo, h);
     } else {
-        qsort(&data[lo], h - lo + 1, sizeof(int), compare);
+        naive_qsort(data, lo, h);
     }
 
     if ((hi - l) > TREESHOLD) {
         #pragma omp task shared(data) firstprivate(l, hi)
         par_qsort(data, l, hi);
     } else {
-        qsort(&data[l], hi - l + 1, sizeof(int), compare);
+        naive_qsort(data, l, hi);
     }
 
     #pragma omp taskwait
 }
 
 int main() {
-    int n = 60;
+    int n = 600000;
     int *data = malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) {
         data[i] = rand() % 100;
@@ -51,7 +73,7 @@ int main() {
     double start, end;
     start = omp_get_wtime();
 
-    #pragma omp parallel shared(data) num_threads(4)
+    #pragma omp parallel shared(data) num_threads(8)
     {
         #pragma omp single
         par_qsort(data, 0, n - 1);
@@ -61,9 +83,9 @@ int main() {
 
     printf("Time: %f\n", end - start);
 
-    for (int i = 0; i < n; i++) {
-        printf("%d ", data[i]);
-    }
+    // for (int i = 0; i < n; i++) {
+    //     printf("%d ", data[i]);
+    // }
     
 
     free(data);
